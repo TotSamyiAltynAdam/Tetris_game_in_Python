834d3d7b67b1f787af109a355730fd4f1d0cf4dd
Yeldos Anarbayev <eldosanarbaev7@gmail.com>
2023-05-25 20:33:46 +0900

    I defined a variable called init_pos_offset by giving to this the half of screen for putting shape on the center of the screen. And then in the tetromino class, in the Block class I changed the previous position to the new by using a that new variable.

--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 import pygame as pg
 
+vec = pg.math.Vector2
+
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
 TILE_SIZE = 50
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 14
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
 
 TETRMONIOES = {
     'T' : [(0,0),(-1,0),(1,0),(0,-1)],
